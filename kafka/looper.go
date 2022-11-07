bf465694b7b7513fc933d40a4a2b4280a00e6641
youyou <705592307@qq.com>
2022-11-07 22:25:42 +0800

    feat: ✨可选项模式实现looper

--- kafka/looper.go
+++ kafka/looper.go
@@ -13,12 +13,17 @@ import (
 	"github.com/YAOYouYou/go-toolkit/logging"
 )
 
+type Option func(l *Looper)
+
 type Looper struct {
-	consumer   kafka.Consumer
-	topic      string
+	consumer *kafka.Consumer
+	topic    string
+
+	numPerCommit int
+	workers      int
+
 	sigC       chan os.Signal
 	eventCb    func(e *kafka.Message) error
-	workers    int
 	count      int
 	gen        int32
 	offsetsMap map[string]kafka.TopicPartition
@@ -27,6 +32,53 @@ type Looper struct {
 	filter  func(m *kafka.Message) bool
 	errorCb func(m *kafka.Message, err error) error
 	closeCb func()
+}
+
+func WithConsumer(consumer *kafka.Consumer) Option {
+	return func(l *Looper) {
+		l.consumer = consumer
+	}
+}
+
+// WithTopic set consumer consume topic
+func WithTopic(topic string) Option {
+	return func(l *Looper) {
+		l.topic = topic
+	}
+}
+
+// WithCommit mean commit every number of messages processed
+// numPerComit must be a multiple of workers
+func WithCommit(num int) Option {
+	return func(l *Looper) {
+		l.numPerCommit = num
+	}
+}
+
+// WithWorkers set number of worker(concuurency)
+// numPerComit must be a multiple of workers
+func WithWorkers(num int) Option {
+	return func(l *Looper) {
+		l.workers = num
+	}
+}
+
+func New(options ...Option) *Looper {
+	l := &Looper{}
+	for _, f := range options {
+		f(l)
+	}
+
+	if l.numPerCommit == 0 {
+		l.numPerCommit = 1
+	}
+	if l.workers == 0 {
+		l.workers = 1
+	}
+	if l.numPerCommit%l.workers != 0 {
+		panic("numPerCommit must be s multiple of workers")
+	}
+	return l
 }
 
 func (l *Looper) Run() {
@@ -94,16 +146,17 @@ Loop:
 					}
 				}(e, gen)
 
-				if l.count%l.workers == 0 {
+				if l.count%l.numPerCommit == 0 {
 					l.wg.Wait()
 					logging.Infof("receive mesaage count: %d", l.count)
 					l.commit(gen)
 				}
 			default:
-				logging.Debugf("other events:", e)
+				logging.Infof("other events:", e)
 			}
 		}
 	}
+
 }
 
 func (l *Looper) commit(gen int32) {
